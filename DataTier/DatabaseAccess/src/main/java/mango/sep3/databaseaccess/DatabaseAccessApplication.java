33c8decabe9dc49cc39b9723f303414d57607afb
Uafa <315211@via.dk>
2022-10-28 13:41:40 +0200

    Initial commit

---
+++ DataTier/DatabaseAccess/src/main/java/mango/sep3/databaseaccess/DatabaseAccessApplication.java
@@ -0,0 +1,14 @@
+package mango.sep3.databaseaccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class DatabaseAccessApplication
+{
+
+  public static void main(String[] args)
+  {
+    SpringApplication.run(DatabaseAccessApplication.class, args);
+  }
+
+}
---
+++ DataTier/DatabaseAccess/src/test/java/mango/sep3/databaseaccess/DatabaseAccessApplicationTests.java
@@ -0,0 +1,13 @@
+package mango.sep3.databaseaccess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class DatabaseAccessApplicationTests
+{
+
+  @Test void contextLoads()
+  {
+  }
+
+}

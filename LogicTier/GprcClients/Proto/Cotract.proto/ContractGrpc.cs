d093cc7f223accf5cd96994698cad9e6af931617
Agata Rudol <315233@via.dk>
2022-10-31 16:26:21 +0100

    Create offer feature implemented in Logic and Database Tier

--- LogicTier/GprcClients/Proto/Cotract.proto/ContractGrpc.cs
+++ LogicTier/GprcClients/Proto/Cotract.proto/ContractGrpc.cs
@@ -117,4 +117,114 @@ public static partial class FarmService
   }
 
 }
+public static partial class OfferService
+{
+  static readonly string __ServiceName = "OfferService";
+
+  [global::System.CodeDom.Compiler.GeneratedCode("grpc_csharp_plugin", null)]
+  static void __Helper_SerializeMessage(global::Google.Protobuf.IMessage message, grpc::SerializationContext context)
+  {
+    #if !GRPC_DISABLE_PROTOBUF_BUFFER_SERIALIZATION
+    if (message is global::Google.Protobuf.IBufferMessage)
+    {
+      context.SetPayloadLength(message.CalculateSize());
+      global::Google.Protobuf.MessageExtensions.WriteTo(message, context.GetBufferWriter());
+      context.Complete();
+      return;
+    }
+    #endif
+    context.Complete(global::Google.Protobuf.MessageExtensions.ToByteArray(message));
+  }
+
+  [global::System.CodeDom.Compiler.GeneratedCode("grpc_csharp_plugin", null)]
+  static class __Helper_MessageCache<T>
+  {
+    public static readonly bool IsBufferMessage = global::System.Reflection.IntrospectionExtensions.GetTypeInfo(typeof(global::Google.Protobuf.IBufferMessage)).IsAssignableFrom(typeof(T));
+  }
+
+  [global::System.CodeDom.Compiler.GeneratedCode("grpc_csharp_plugin", null)]
+  static T __Helper_DeserializeMessage<T>(grpc::DeserializationContext context, global::Google.Protobuf.MessageParser<T> parser) where T : global::Google.Protobuf.IMessage<T>
+  {
+    #if !GRPC_DISABLE_PROTOBUF_BUFFER_SERIALIZATION
+    if (__Helper_MessageCache<T>.IsBufferMessage)
+    {
+      return parser.ParseFrom(context.PayloadAsReadOnlySequence());
+    }
+    #endif
+    return parser.ParseFrom(context.PayloadAsNewBuffer());
+  }
+
+  [global::System.CodeDom.Compiler.GeneratedCode("grpc_csharp_plugin", null)]
+  static readonly grpc::Marshaller<global::Offer> __Marshaller_Offer = grpc::Marshallers.Create(__Helper_SerializeMessage, context => __Helper_DeserializeMessage(context, global::Offer.Parser));
+
+  [global::System.CodeDom.Compiler.GeneratedCode("grpc_csharp_plugin", null)]
+  static readonly grpc::Method<global::Offer, global::Offer> __Method_CreateOffer = new grpc::Method<global::Offer, global::Offer>(
+      grpc::MethodType.Unary,
+      __ServiceName,
+      "CreateOffer",
+      __Marshaller_Offer,
+      __Marshaller_Offer);
+
+  /// <summary>Service descriptor</summary>
+  public static global::Google.Protobuf.Reflection.ServiceDescriptor Descriptor
+  {
+    get { return global::ContractReflection.Descriptor.Services[1]; }
+  }
+
+  /// <summary>Client for OfferService</summary>
+  public partial class OfferServiceClient : grpc::ClientBase<OfferServiceClient>
+  {
+    /// <summary>Creates a new client for OfferService</summary>
+    /// <param name="channel">The channel to use to make remote calls.</param>
+    [global::System.CodeDom.Compiler.GeneratedCode("grpc_csharp_plugin", null)]
+    public OfferServiceClient(grpc::ChannelBase channel) : base(channel)
+    {
+    }
+    /// <summary>Creates a new client for OfferService that uses a custom <c>CallInvoker</c>.</summary>
+    /// <param name="callInvoker">The callInvoker to use to make remote calls.</param>
+    [global::System.CodeDom.Compiler.GeneratedCode("grpc_csharp_plugin", null)]
+    public OfferServiceClient(grpc::CallInvoker callInvoker) : base(callInvoker)
+    {
+    }
+    /// <summary>Protected parameterless constructor to allow creation of test doubles.</summary>
+    [global::System.CodeDom.Compiler.GeneratedCode("grpc_csharp_plugin", null)]
+    protected OfferServiceClient() : base()
+    {
+    }
+    /// <summary>Protected constructor to allow creation of configured clients.</summary>
+    /// <param name="configuration">The client configuration.</param>
+    [global::System.CodeDom.Compiler.GeneratedCode("grpc_csharp_plugin", null)]
+    protected OfferServiceClient(ClientBaseConfiguration configuration) : base(configuration)
+    {
+    }
+
+    [global::System.CodeDom.Compiler.GeneratedCode("grpc_csharp_plugin", null)]
+    public virtual global::Offer CreateOffer(global::Offer request, grpc::Metadata headers = null, global::System.DateTime? deadline = null, global::System.Threading.CancellationToken cancellationToken = default(global::System.Threading.CancellationToken))
+    {
+      return CreateOffer(request, new grpc::CallOptions(headers, deadline, cancellationToken));
+    }
+    [global::System.CodeDom.Compiler.GeneratedCode("grpc_csharp_plugin", null)]
+    public virtual global::Offer CreateOffer(global::Offer request, grpc::CallOptions options)
+    {
+      return CallInvoker.BlockingUnaryCall(__Method_CreateOffer, null, options, request);
+    }
+    [global::System.CodeDom.Compiler.GeneratedCode("grpc_csharp_plugin", null)]
+    public virtual grpc::AsyncUnaryCall<global::Offer> CreateOfferAsync(global::Offer request, grpc::Metadata headers = null, global::System.DateTime? deadline = null, global::System.Threading.CancellationToken cancellationToken = default(global::System.Threading.CancellationToken))
+    {
+      return CreateOfferAsync(request, new grpc::CallOptions(headers, deadline, cancellationToken));
+    }
+    [global::System.CodeDom.Compiler.GeneratedCode("grpc_csharp_plugin", null)]
+    public virtual grpc::AsyncUnaryCall<global::Offer> CreateOfferAsync(global::Offer request, grpc::CallOptions options)
+    {
+      return CallInvoker.AsyncUnaryCall(__Method_CreateOffer, null, options, request);
+    }
+    /// <summary>Creates a new instance of client from given <c>ClientBaseConfiguration</c>.</summary>
+    [global::System.CodeDom.Compiler.GeneratedCode("grpc_csharp_plugin", null)]
+    protected override OfferServiceClient NewInstance(ClientBaseConfiguration configuration)
+    {
+      return new OfferServiceClient(configuration);
+    }
+  }
+
+}
 #endregion
---
+++ LogicTier/Shared/DTOs/OfferCreationDto.cs
@@ -0,0 +1,23 @@
+﻿namespace Shared.DTOs;
+
+public class OfferCreationDto
+{
+    public string Name { get; set; }
+
+    public int Quantity { get; set; }
+    
+    public string Unit { get; set; }
+
+    public double Price { get; set; }
+
+    public bool Delivery { get; set; }
+
+    public bool PickUp { get; set; }
+    
+    public bool PickYourOwn { get; set; }
+    public string? Description { get; set; }
+
+    public string ImagePath { get; set; }
+    
+    
+}

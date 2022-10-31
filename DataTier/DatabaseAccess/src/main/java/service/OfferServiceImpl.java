b1fa05336d818a1e3249615c7f72dcca5e661a9e
Beatricia Nagit <315186@via.dk>
2022-10-31 09:27:57 +0100

    Implementing OfferServiceImpl

--- DataTier/DatabaseAccess/src/main/java/service/OfferServiceImpl.java
+++ DataTier/DatabaseAccess/src/main/java/service/OfferServiceImpl.java
@@ -1,2 +1,34 @@
-package service;public class OfferServiceImpl {
+package service;
+
+import io.grpc.stub.StreamObserver;
+import mango.sep3.databaseaccess.FileData.FileContext;
+import mango.sep3.databaseaccess.protobuf.Offer;
+import mango.sep3.databaseaccess.protobuf.OfferItems;
+import mango.sep3.databaseaccess.protobuf.OfferServiceGrpc;
+import mango.sep3.databaseaccess.protobuf.Void;
+
+import java.util.Collection;
+
+public class OfferServiceImpl extends OfferServiceGrpc.OfferServiceImplBase
+{
+  private final FileContext fileContext;
+
+  public OfferServiceImpl(FileContext context){
+    fileContext = context;
+  }
+  @Override public void getOffers(Void request,
+      StreamObserver<OfferItems> responseObserver)
+  {
+    OfferItems.Builder response = OfferItems.newBuilder();
+    Collection<Offer> offers = fileContext.Offers();
+
+    for (int i = 0; i < offers.size(); i++)
+    {
+      response.setOffers(offers.iterator().next().getOfferId(), offers.iterator().next());
+    }
+
+    //sending back data to the client
+    responseObserver.onNext(response.build());
+    responseObserver.onCompleted();
+  }
 }
---
+++ LogicTier/Application/LogicImplementations/OfferLogic.cs
@@ -0,0 +1,24 @@
+﻿using Application.DAOInterfaces;
+using Application.LogicInterfaces;
+
+namespace Application.LogicImplementations;
+
+public class OfferLogic : IOfferLogic
+{
+    private OfferService
+
+    public OfferLogic(IOfferDao offerDao)
+    {
+        this.offerDao = offerDao;
+    }
+    
+    public Task CreateAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Offer>> GetAsync()
+    {
+        return offerDao.GetAsync();
+    }
+}

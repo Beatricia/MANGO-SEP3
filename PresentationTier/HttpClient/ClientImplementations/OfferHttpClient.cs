6baec34fcd672f229fd1bf6c30a072ddc521e562
simonfrayer <315183@via.dk>
2022-11-01 12:06:45 +0100

    Create Offer basic layout

---
+++ PresentationTier/HttpClient/ClientImplementations/OfferHttpClient.cs
@@ -0,0 +1,24 @@
+﻿using HttpClient.ClientInterfaces;
+using Shared.Models;
+
+
+namespace HttpClient.ClientImplementations;
+
+public class OfferHttpClient : IOfferService
+{
+    /*private readonly HttpClient client;
+
+    public OfferHttpClient(HttpClient client)
+    {
+        this.client = client;
+    } */
+    public Task CreateAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ICollection<Offer>> GetAsync()
+    {
+        throw new NotImplementedException();
+    }
+}

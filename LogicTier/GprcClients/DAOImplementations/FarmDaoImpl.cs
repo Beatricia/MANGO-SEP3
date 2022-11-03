43944ef406dc40342822fd2be8c16b54ac5eae3c
Uafa <315211@via.dk>
2022-11-03 14:02:56 +0100

    Created Dao implementations

---
+++ LogicTier/GprcClients/DAOImplementations/FarmDaoImpl.cs
@@ -0,0 +1,31 @@
+﻿using Application.DAOInterfaces;
+
+namespace GprcClients.DAOImplementations;
+
+public class FarmDaoImpl : IFarmDao
+{
+    private FarmService.FarmServiceClient farmServiceClient;
+
+    public FarmDaoImpl(FarmService.FarmServiceClient farmServiceClient)
+    {
+        this.farmServiceClient = farmServiceClient;
+    }
+
+    public async Task<Shared.Models.Farm> CreateAsync(Shared.Models.Farm farm)
+    {
+        var toCreate = new Farm
+        {
+            Name = farm.Name,
+            Phone = farm.Phone,
+            Zip = farm.Address.ZIP,
+            Address = farm.Address.Street,
+            City = farm.Address.City,
+            DeliveryDistance = farm.DeliveryDistance,
+            FarmStatus = farm.FarmStatus,
+        };
+
+        _ = await farmServiceClient.CreateFarmAsync(toCreate);
+        
+        return farm;
+    }
+}
---
+++ LogicTier/GprcClients/DAOImplementations/OfferDaoImpl.cs
@@ -0,0 +1,62 @@
+﻿using Application.DAOInterfaces;
+
+namespace GprcClients.DAOImplementations;
+
+public class OfferDaoImpl : IOfferDao
+{
+    private OfferService.OfferServiceClient offerService;
+
+    public OfferDaoImpl(OfferService.OfferServiceClient offerService)
+    {
+        this.offerService = offerService;
+    }
+    
+    
+    public async Task CreateAsync(Shared.Models.Offer offer)
+    {
+        var offerToCreate = new Offer
+        {
+            Id = offer.Id,
+            Name = offer.Name,
+            Quantity = offer.Quantity,
+            Unit = offer.Unit,
+            Price = offer.Price,
+            Delivery = offer.Delivery,
+            PickUp = offer.PickUp,
+            PickYourOwn = offer.PickYourOwn,
+            Description = offer.Description,
+            ImagePath = offer.ImagePath
+        };
+        
+        _ = await offerService.CreateOfferAsync(offerToCreate);
+    }
+
+    public async Task<IEnumerable<Shared.Models.Offer>> GetAsync()
+    {
+        OfferItems offersBuff = await offerService.GetOffersAsync(new Void());
+
+        var list = new List<Shared.Models.Offer>();
+        foreach (var created in offersBuff.Offers)
+        {
+            if (created is null)
+                continue;
+
+            Shared.Models.Offer offerToPresentationTier = new Shared.Models.Offer
+            {
+                Id = created.Id,
+                Name = created.Name,
+                Quantity = created.Quantity,
+                Unit = created.Unit,
+                Price = created.Price,
+                Delivery = created.Delivery,
+                PickUp = created.PickUp,
+                PickYourOwn = created.PickYourOwn,
+                Description = created.Description,
+                ImagePath = created.ImagePath
+            };
+            list.Add(offerToPresentationTier);
+        }
+
+        return list;
+    }
+}

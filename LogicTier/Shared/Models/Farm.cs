c6393d4639754b480da7d61ea98116e43cd5ee43
Uafa <315211@via.dk>
2022-11-03 14:07:02 +0100

    changing nullable type for delivery distance

--- LogicTier/Shared/Models/Farm.cs
+++ LogicTier/Shared/Models/Farm.cs
@@ -5,11 +5,11 @@ public class Farm
     //todo should the name be unique or should we assign an ID?
     public string Name { get; set; }
 
-    public FarmAddress Address { get; set; }
+    public Address Address { get; set; }
     
     public string Phone { get; set; }
     
-    public int? DeliveryDistance { get; set; }
+    public int DeliveryDistance { get; set; }
     
     public string? FarmStatus { get; set; }
 }

452a2c0b4ca141616ccf1e69b120e25b44d5c144
Enbi81 <wotng2002@gmail.com>
2022-11-23 20:36:09 +0100

    setting up icons

---
+++ LogicTier/WebAPI/Utils/FarmIconResource.cs
@@ -0,0 +1,28 @@
+﻿namespace WebAPI.Utils;
+
+public class FarmIconResource
+{
+    // TODO: mention the source of the icons in the docs somewhere: https://www.flaticon.com/free-icons/farm
+    
+    public const string BaseFolder = "wwwroot/images/farmicons/";
+
+    public ICollection<string> AllIcons
+    {
+        get
+        {
+            Directory.CreateDirectory(BaseFolder);
+        
+            // get all farm icons from wwwroot/images/farmicons
+            ICollection<string> icons = Directory.GetFiles(BaseFolder, "*.png", SearchOption.TopDirectoryOnly)
+                .Select(Path.GetFileName)
+                .Where(path => path is not null)
+                .Cast<string>()
+                .Select(path => Path.Combine(BaseFolder, path))
+                .ToList();
+
+            return icons;
+        }
+    }
+    
+    public string DefaultIcon => Path.Combine(BaseFolder, "default.png");
+}

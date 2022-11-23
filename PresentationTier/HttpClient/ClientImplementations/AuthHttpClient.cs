f386dedfa53f356b2d992cd9633387a754302b8c
Enbi81 <wotng2002@gmail.com>
2022-11-23 15:36:03 +0100

    presentation tier of registering done

---
+++ PresentationTier/HttpClient/ClientImplementations/AuthHttpClient.cs
@@ -0,0 +1,47 @@
+﻿using System.Net.Http.Json;
+using HttpClient.ClientInterfaces;
+using Shared.DTOs;
+using Shared.Models;
+
+namespace HttpClient.ClientImplementations;
+
+public class AuthHttpClient : IAuthService
+{
+    // accept httpclient from constructor to field
+    private readonly System.Net.Http.HttpClient client;
+    
+    public AuthHttpClient(System.Net.Http.HttpClient httpClient)
+    {
+        client = httpClient;
+    }
+    
+    
+    public Task<User> LoginAsync(string username, string password)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<User> RegisterAsync(string username, string password, bool isFarmer)
+    {
+        RegisterDto dto = new RegisterDto()
+        {
+            Username = username,
+            Password = password,
+            IsFarmer = isFarmer
+        };
+        
+        
+        HttpResponseMessage response = await client.PostAsJsonAsync("/auth/register", dto);
+        if (!response.IsSuccessStatusCode)
+        {
+            string content = await response.Content.ReadAsStringAsync();
+            throw new Exception(content);
+        }
+        
+        var user = await response.Content.ReadFromJsonAsync<User>();
+        if(user == null)
+            throw new Exception("Cannot read user from response");
+        
+        return user;
+    }
+}
---
+++ PresentationTier/HttpClient/ClientInterfaces/IAuthService.cs
@@ -0,0 +1,9 @@
+﻿using Shared.Models;
+
+namespace HttpClient.ClientInterfaces;
+
+public interface IAuthService
+{
+    Task<User> LoginAsync(string username, string password);
+    Task<User> RegisterAsync(string username, string password, bool isFarmer);
+}

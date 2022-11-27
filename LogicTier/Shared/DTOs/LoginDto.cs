0ecdbd6dc2f0a672f747b4e1bdd8e1dac29b08cf
Enbi81 <wotng2002@gmail.com>
2022-11-27 20:12:10 +0100

    adding LoginDto.cs, idk why was it missing

---
+++ LogicTier/Shared/DTOs/LoginDto.cs
@@ -0,0 +1,16 @@
+﻿namespace Shared.DTOs;
+
+/// <summary>
+/// Holds information required to log in
+/// </summary>
+public class LoginDto
+{
+    /// <summary>
+    /// Username of the person who wants to log in
+    /// </summary>
+    public string Username { get; set; }
+    /// <summary>
+    /// Password of the person who wants to log in
+    /// </summary>
+    public string Password { get; set; }
+}

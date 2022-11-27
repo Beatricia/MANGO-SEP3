0a4e4bb2b931b617cab764b0241b27db465053ff
simonfrayer <315183@via.dk>
2022-11-27 22:31:13 +0100

    some unversioned files just trying to resolve something in git

---
+++ LogicTier/Shared/Auth/AuthorizationPolicies.cs
@@ -0,0 +1,28 @@
+using System.Security.Claims;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Shared.Auth;
+
+public static class AuthorizationPolicies
+{
+    public static void AddPolicies(IServiceCollection services)
+    {
+        services.AddAuthorizationCore(options =>
+        {
+            //define our own policies below ---------------------------------------
+            
+            /*
+            options.AddPolicy("MustBeTeacher", a =>
+                a.RequireAuthenticatedUser().RequireClaim("Role", "Teacher"));
+    
+            options.AddPolicy("SecurityLevel2OrAbove", a =>
+                a.RequireAuthenticatedUser().RequireAssertion(context =>
+                {
+                    Claim? levelClaim = context.User.FindFirst(claim => claim.Type.Equals("SecurityLevel"));
+                    if (levelClaim == null) return false;
+                    return int.Parse(levelClaim.Value) >= 2;
+                }));
+                */
+        });
+    }
+}

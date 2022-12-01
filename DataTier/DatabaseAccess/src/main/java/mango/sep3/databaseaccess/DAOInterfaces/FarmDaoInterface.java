5d880c5154dc819a15d725ecbcc188bea2ad5755
Uafa <315211@via.dk>
2022-12-01 11:08:16 +0100

    FarmDaoInterface

--- DataTier/DatabaseAccess/src/main/java/mango/sep3/databaseaccess/DAOInterfaces/FarmDaoInterface.java
+++ DataTier/DatabaseAccess/src/main/java/mango/sep3/databaseaccess/DAOInterfaces/FarmDaoInterface.java
@@ -10,4 +10,5 @@ public interface FarmDaoInterface
   Farm createFarm(Farm farm);
   Farm getFarmByName(String farmName);
   Collection<Farm> getFarms(Farmer farmer);
+  Farm updateFarm(String name, String status);
 }

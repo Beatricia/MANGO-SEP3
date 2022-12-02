1e3f8b82ebe8d8af38c4e834a9fd4c0b8f316e63
Agata Rudol <315233@via.dk>
2022-12-02 20:17:41 +0100

    Create order is fixed

--- DataTier/DatabaseAccess/src/main/java/mango/sep3/databaseaccess/Repositories/CartRepository.java
+++ DataTier/DatabaseAccess/src/main/java/mango/sep3/databaseaccess/Repositories/CartRepository.java
@@ -2,7 +2,9 @@ package mango.sep3.databaseaccess.Repositories;
 
 import mango.sep3.databaseaccess.Shared.CartItem;
 import mango.sep3.databaseaccess.Shared.Customer;
+import mango.sep3.databaseaccess.Shared.Offer;
 import mango.sep3.databaseaccess.Shared.User;
+import org.hibernate.annotations.Where;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,5 +14,6 @@ import java.util.Collection;
 public interface CartRepository extends JpaRepository<CartItem,Integer>
 {
   Collection<CartItem> findAllByCustomer(Customer username);
+  Collection<CartItem> findAllByCustomerAndCollectionOption(Customer username, String collectionOption);
   void deleteAllByCustomer(Customer username);
 }

b2e083cb38bb6e6d5c8f4bee5215aad54b41e3ee
Enbi81 <wotng2002@gmail.com>
2022-12-04 20:39:13 +0100

    Adding Review.cs

---
+++ LogicTier/Shared/Models/Review.cs
@@ -0,0 +1,21 @@
+﻿namespace Shared.Models;
+
+public class Review
+{
+    /// <summary>
+    /// Id of the offer the review was given for
+    /// </summary>
+    public int OfferId { get; set; }
+    /// <summary>
+    /// Username of the user who gave the review
+    /// </summary>
+    public string WrittenBy { get; set; } = "";
+    /// <summary>
+    /// Content of the review
+    /// </summary>
+    public string Content { get; set; } = "";
+    /// <summary>
+    /// The Time the review was published or updated
+    /// </summary>
+    public DateTime Date { get; set; }
+}

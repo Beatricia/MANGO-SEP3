6dbb0ffdad5ebc32f454cd87dd66160bce9f33fc
Enbi81 <wotng2002@gmail.com>
2022-12-12 13:39:01 +0100

    finished the reporting in the presentation (it looks super cool)

--- PresentationTier/HttpClient/ClientImplementations/ReportHttpClient.cs
+++ PresentationTier/HttpClient/ClientImplementations/ReportHttpClient.cs
@@ -1,12 +1,16 @@
-﻿using System.Text;
+﻿using System.Net.Http.Json;
+using System.Text;
 using System.Text.Json;
 using HttpClient.ClientInterfaces;
+using Shared.DTOs;
 using Shared.Models;
 
 namespace HttpClient.ClientImplementations;
 
 public class ReportHttpClient : IReportService
 {
+    private const string Endpoint = "/reports";
+    
     private System.Net.Http.HttpClient client => _apiAccess.HttpClient;
     private ApiAccess _apiAccess;
 
@@ -17,7 +21,7 @@ public class ReportHttpClient : IReportService
     
     public async Task<ICollection<Report>> GetAllReports()
     {
-        HttpResponseMessage response = await client.GetAsync("/report"); 
+        HttpResponseMessage response = await client.GetAsync(Endpoint); 
         string content = await response.Content.ReadAsStringAsync();
 
         if (!response.IsSuccessStatusCode)
@@ -35,7 +39,7 @@ public class ReportHttpClient : IReportService
 
     public async Task IgnoreReportAsync(long id)
     {
-        HttpResponseMessage response = await client.DeleteAsync($"/Report/{id}");
+        HttpResponseMessage response = await client.DeleteAsync($"{Endpoint}/{id}");
         if (!response.IsSuccessStatusCode)
         {
             string content = await response.Content.ReadAsStringAsync();
@@ -45,10 +49,20 @@ public class ReportHttpClient : IReportService
 
     public async Task NotifyFarmerAsync(long id)
     {
-        HttpResponseMessage response = await client.PostAsync($"/Report/{id}", null);
+        HttpResponseMessage response = await client.PostAsync($"{Endpoint}/{id}", null);
         if (!response.IsSuccessStatusCode)
         {
             string content = await response.Content.ReadAsStringAsync();
+            throw new Exception(content);
+        }
+    }
+
+    public async Task ReportOfferAsync(ReportCreationDto dto)
+    {
+        HttpResponseMessage responseMessage = await client.PostAsJsonAsync(Endpoint, dto);
+        if (!responseMessage.IsSuccessStatusCode)
+        {
+            string content = await responseMessage.Content.ReadAsStringAsync();
             throw new Exception(content);
         }
     }
